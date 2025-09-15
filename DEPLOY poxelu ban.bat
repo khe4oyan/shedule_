01682e13ef2798f8aa878ec88a2c2dedd34b386e
khe4oyan <khe4oyan@gmail.com>
2025-09-15 23:10:42 +0400

    Elya update

--- DEPLOY poxelu ban.bat
+++ DEPLOY poxelu ban.bat
@@ -1,6 +1,16 @@
+git pull
+
 git add .
 git commit -m "Elya update"
 git push
 npm run deploy
 
-echo OK
+cls
+echo ■────────────────────┐
+echo | Vsyo, karas pakes  |
+echo └────────────────────■
+echo ""
+echo ""
+echo ""
+
+pause
