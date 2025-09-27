caf087474555daaf7901f7d54e58047e6b6ba1f3
Suren <khe4oyan@gmail.com>
2025-09-27 14:43:42 +0400

    .

--- src/components/ShowWeekLessons/ShowWeekLessons.jsx
+++ src/components/ShowWeekLessons/ShowWeekLessons.jsx
@@ -1,5 +1,5 @@
 // libs
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 // components
 import Lesson from '../Lesson';
@@ -11,35 +11,38 @@ import lessons from '../../constants/lessons';
 import classes from './styles.module.css';
 
 export default function ShowWeekLessons() {
-  const [daySheduleNum, setDaySheduleNum] = useState(() => {
+  const initState = useCallback(() => {
     const dayNum = new Date().getDay() - 1;
-      
-    if ( dayNum < 0 || dayNum > 5) return -1;
+
+    if (dayNum < 0 || dayNum > 4) return -1;
 
     return dayNum;
   });
 
-  const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const [dayscheduleNum, setDayscheduleNum] = useState(initState);
+
+  const days = ["Երկ", "Երեք", "Չորք", "Հինգ", "Ուրբ"];
 
   return (
     <div className={classes.root}>
-      <h2>Full schedule</h2>
-      
+      <h2>Ամբողջական գրաֆիկ</h2>
+
       <div className={classes.buttons}>
         {
           days.map((day, ind) =>
-            <button key={day} className={`${classes.button} ${ind === daySheduleNum && classes.buttonActive}`} onClick={() => setDaySheduleNum(ind)}>{day}</button>
+            <button key={day} className={`${classes.button} ${ind === dayscheduleNum && classes.buttonActive}`} onClick={() => setDayscheduleNum(ind)}>{day}</button>
           )
         }
       </div>
 
       <div className={classes.lessons}>
         {
-          lessons[daySheduleNum]?.map((lesson, ind) =>
-            <Lesson
-              data={lesson}
-              key={ind}
-            />
+          lessons[dayscheduleNum]?.map(
+            (lesson, ind) =>
+              <Lesson
+                data={{ ...lesson?.getData(), status: "default" }}
+                key={`${lesson.title} ${ind}`}
+              />
           )
         }
       </div>
